da3102dd117a67a9083570ea139cb41065b9558e
TMN <tommy.martin@adsoftware.fr>
2025-05-17 09:20:36 -0500

    facicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,29 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Inscription FIS",
   description: "Inscription FIS",
+  icons: {
+    icon: [
+      {url: "/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png"},
+      {url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png"},
+    ],
+    apple: {
+      url: "/favicons/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    other: [
+      {
+        url: "/favicons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
